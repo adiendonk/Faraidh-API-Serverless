d0f0a7a9c23addabe9de52d39f7c375d0e3ddd40
Adhi Indi Arysanto <adie_ndonk@yahoo.co.id>
2022-05-22 16:25:09 +0700

    get format

--- controllers/solvingController.js
+++ controllers/solvingController.js
@@ -1,13 +1,14 @@
 const SolveModel = require('../Models/solvingModel');
 
 class Controller {
-    static getFormat( req, res ) {
-        res.status(200).json({ ahli_waris: new SolveModel(), total_harta: 0 });
+    static getFormat() {
+        return({ ahli_waris: new SolveModel(), total_harta: 0 });
     }
 
     static solving( req, res ) {
         const result = SolveModel.solving(req.body);
-        res.status(200).json({ penerima_waris: result });
+        //res.status(200).json({ penerima_waris: result });
+        return({ penerima_waris: result });
     }
 }
 
--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const app = express(), port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-app.use(require('./router'));
-
-const serverlessExpress = require('aws-serverless-express');
-const server = serverlessExpress.createServer(app);
-exports.handler  = (event, context) => serverlessExpress.proxy(server, event, context)
-
-//app.listen(port, () => console.log(`Running in port ${port}...`));
+const Controller = require('./controllers/solvingController');
+console.log(Controller.getFormat())
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: Controller.getFormat,
+    };
+    return response;
+};

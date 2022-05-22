6b8f96940362af585ba61215fa13e3dffb81d007
Adhi Indi Arysanto <adie_ndonk@yahoo.co.id>
2022-05-22 21:59:49 +0700

    test again

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ exports.handler = async (event) => {
     // TODO implement
     var response;
     if (event.body !== null && event.body !== undefined) {
-        const body = JSON.parse(event.body) 
+        //const body = JSON.parse(event.body);
+        const body = event.body;
 
         response = {
             statusCode: 200,

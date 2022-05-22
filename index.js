8e07f87777db48cfc90163afb4932dbb726134e3
Adhi Indi Arysanto <adie_ndonk@yahoo.co.id>
2022-05-22 21:58:03 +0700

    test event body

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const Controller = require('./controllers/solvingController');
+const router = require('./router/solving');
 
 exports.handler = async (event) => {
     // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(Controller.getFormat()),
-    };
+    var response;
+    if (event.body !== null && event.body !== undefined) {
+        const body = JSON.parse(event.body) 
+
+        response = {
+            statusCode: 200,
+            body: body,
+        };
+    }else{
+        response = {
+            statusCode: 200,
+            body: JSON.stringify(Controller.getFormat()),
+        };
+    }
+    
     return response;
 };
--- router/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-router.use('/solving', require('./solving'));
-
-module.exports = router;
